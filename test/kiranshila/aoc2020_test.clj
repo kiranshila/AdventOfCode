a9bddf213324f6215cc643a8f56b854e4b600ecf
Kiran Shila <me@kiranshila.com>
2020-12-02 02:12:40 -0500

    Remove a folder

--- test/kiranshila/aoc2020_test.clj
+++
@@ -1,7 +0,0 @@
-(ns kiranshila.aoc2020-test
-  (:require [clojure.test :refer :all]
-            [kiranshila.aoc2020 :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
