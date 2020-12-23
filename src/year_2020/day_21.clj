692ee5f5addc57997b34b77e9fc6619d00f510a4
Kiran Shila <me@kiranshila.com>
2020-12-22 23:40:36 -0500

    Day 22

--- src/year_2020/day_21.clj
+++ src/year_2020/day_21.clj
@@ -1,56 +1,71 @@
-(ns year-2020.day-21
+(ns year-2020.day-22
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
-            [clojure.set :as set]))
+            [clojure.edn :as edn]
+            [clojure.pprint :refer [pprint]]))
 
-(def input (slurp (io/resource "2020/21/input")))
+(def input (slurp (io/resource "2020/22/input")))
 
-(defn parse-ingredients [line]
-  (let [[ingreds alergens] (str/split line #"\(contains ")
-        ingreds (into #{} (map keyword (str/split (str/trim ingreds) #" ")))
-        alergens (into #{} (map keyword (map str/trim (str/split (apply str (butlast alergens)) #","))))]
-    [ingreds alergens]))
+(def queue clojure.lang.PersistentQueue/EMPTY)
 
-(defn solved? [alergen-map]
-  (apply (partial = 1) (map count (vals alergen-map))))
+(defn parse-cards [input]
+  (->> (re-seq #"Player \d:\n([\d?\n]+)" input)
+       (map second)
+       (map str/split-lines)
+       (map #(map edn/read-string %))
+       (map #(into queue %))))
 
-(defn collect-solved [alergen-map]
-  (apply set/union (map second (filter (fn [[k v]] (= 1 (count v))) alergen-map))))
+(defn score [deck]
+  (reduce + (map * deck (range (count deck) 0 -1))))
 
-(defn solve [alergen-map]
-  (loop [alergen-map alergen-map]
-    (if (solved? alergen-map)
-      alergen-map
-      (let [solved (collect-solved alergen-map)]
-        (recur (into {} (map (fn [[k v]] [k (if (= 1 (count v)) v (set/difference v solved))]) alergen-map)))))))
-
-(defn solve-alergens [ingredients]
-  (let [alergens (apply  set/union (map second ingredients))
-        alergen-map (apply merge (for [alergen alergens]
-                                   {alergen (->> ingredients
-                                                 (filter #(contains? (second %) alergen))
-                                                 (map first)
-                                                 (apply set/intersection))}))]
-    (solve alergen-map)))
+(defn play-combat [[p1 p2]]
+  (loop [p1 p1
+         p2 p2]
+    (cond
+      (empty? p1) p2
+      (empty? p2) p1
+      :else
+      (let [p1-top (peek p1)
+            p2-top (peek p2)]
+        (if (> p1-top p2-top)
+          (recur (conj (pop p1) p1-top p2-top) (pop p2))
+          (recur (pop p1) (conj (pop p2) p2-top p1-top)))))))
 
 (defn solution-1 [input]
-  (let [lines (str/split-lines input)
-        ingredients (map parse-ingredients lines)
-        ingredient-alergen-map (solve-alergens ingredients)
-        alergic-ingredients (apply set/union (vals ingredient-alergen-map))]
-    (->> ingredients
-         (map first)
-         (map #(set/difference % alergic-ingredients))
-         (map count)
-         (reduce +))))
+  (->> input
+       parse-cards
+       play-combat
+       score))
+
+(defn take-queue [n q]
+  (into queue (take n q)))
+
+(defn play-recursive-combat [[p1 p2]]
+  (loop [p1 p1
+         p2 p2
+         history #{}]
+    (cond
+      (empty? p1) [:p2 p2]
+      (empty? p2) [:p1 p1]
+      (contains? history [p1 p2]) [:p1 p1]
+      :else
+      (let [p1-top (peek p1)
+            p2-top (peek p2)]
+        (if (if (and (> (count p1) p1-top)
+                     (> (count p2) p2-top))
+              (= :p1 (first (play-recursive-combat [(take-queue p1-top (pop p1))
+                                                    (take-queue p2-top (pop p2))])))
+              (> p1-top p2-top))
+          (recur (conj (pop p1) p1-top p2-top)
+                 (pop p2)
+                 (conj history [p1 p2]))
+          (recur (pop p1)
+                 (conj (pop p2) p2-top p1-top)
+                 (conj history [p1 p2])))))))
 
 (defn solution-2 [input]
-  (let [lines (str/split-lines input)
-        ingredients (map parse-ingredients lines)
-        ingredient-alergen-map (solve-alergens ingredients)]
-    (->> ingredient-alergen-map
-         (into (sorted-map))
-         vals
-         (map first)
-         (map name)
-         (str/join ","))))
+  (->> input
+       parse-cards
+       play-recursive-combat
+       second
+       score))
