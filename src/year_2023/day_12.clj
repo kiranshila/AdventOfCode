97bd2c33409a64413ac99ac5d3692c4c43798427
Kiran Shila <me@kiranshila.com>
2023-12-12 13:36:42 -0800

    Day 12

--- src/year_2023/day_12.clj
+++ src/year_2023/day_12.clj
@@ -1,7 +1,6 @@
 (ns year-2023.day-12
   (:require
    [clojure.java.io :as io]
-   [clojure.set :as set]
    [clojure.string :as str]))
 
 (def input (slurp (io/resource "2023/12/input")))
@@ -12,34 +11,31 @@
         groups (->> (str/split groups #",") (mapv parse-long))]
     [record groups]))
 
-(defn matches
-  ([record groups] (matches record groups 0 []))
-  ([record groups curr-count trail]
-   (if (not (seq record))
-     (when (or (and (= 0 (count groups)) (= 0 curr-count))
+(def matches
+  (memoize
+   (fn [record groups curr-count]
+     (if (not (seq record))
+       (if (or (and (= 0 (count groups)) (= 0 curr-count))
                (and (= 1 (count groups)) (= curr-count (first groups))))
-       (apply str trail))
-     (let [head (first record)]
-       (case head
-         \# (recur (rest record) groups (inc curr-count) (conj trail head))
-         \. (cond
-              (zero? curr-count) (recur (rest record) groups 0 (conj trail head))
-              (= curr-count (first groups)) (recur (rest record) (rest groups) 0 (conj trail head))
-              :else nil)
-         \? [(matches (conj (rest record) \#) groups curr-count trail)
-             (matches (conj (rest record) \.) groups curr-count trail)])))))
-
-(defn count-matches [[record groups]]
-  (->> (matches record groups)
-       flatten
-       (filter some?)
-       count))
-
-(defn part-one [input]
-  (->> (str/split-lines input)
-       (map parse-line)
-       (map count-matches)
-       (reduce +)))
+         1 0)
+       (let [head (first record)]
+         (case head
+           \# (matches (rest record) groups (inc curr-count))
+           \. (cond
+                (zero? curr-count) (matches (rest record) groups 0)
+                (= curr-count (first groups)) (matches (rest record) (rest groups) 0)
+                :else 0)
+           \? (+ (matches (conj (rest record) \#) groups curr-count)
+                 (matches (conj (rest record) \.) groups curr-count))))))))
 
 (defn unfold [[record groups]]
-  )
+  (let [rr (interpose \? (take 5 (cycle (list record))))
+        rg (take 5 (cycle (list groups)))]
+    [(apply str rr) (into [] (flatten rg))]))
+
+(defn solve [input]
+  (->> (str/split-lines input)
+       (map parse-line)
+       #_(map unfold)
+       (map #(matches (first %) (second %) 0))
+       (reduce +)))
