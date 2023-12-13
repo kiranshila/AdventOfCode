c912fa7520ed738e5bbfd433d075ff9dd286a3d5
Kiran Shila <me@kiranshila.com>
2023-12-12 16:57:27 -0800

    Rough day 10

--- src/year_2023/day_10.clj
+++ src/year_2023/day_10.clj
@@ -51,14 +51,98 @@
 (defn trace-path [pipe-map]
   (let [start (first (keep (fn [[k v]] (when (= \S v) k)) pipe-map))]
     (loop [visited #{start}
+           path [[start (pipe-map start)]]
            current (next-start pipe-map start)]
       (if-let [next (next-valid pipe-map visited current)]
-        (recur (conj visited current) next)
-        visited))))
+        (recur (conj visited current) (conj path [current (pipe-map current)]) next)
+        (conj path [current (pipe-map current)])))))
 
 (defn part-one [input]
   (->> (parse-pipes input)
        trace-path
        count
-       (#(quot % 2))
-       inc))
+       (#(quot % 2))))
+
+(def foo (->> (parse-pipes example) trace-path))
+
+; Using the second derivative
+(defn normals [path]
+  (let [path (map first path)
+        wrap (conj (into [] (conj path (last path))) (first path))]
+    (into {}
+          (for [i (range 1 (inc (count path)))
+                :let [pos (nth wrap i)
+                      [pr pc] (nth wrap (dec i))
+                      [nr nc] (nth wrap (inc i))
+                      dr (- nr pr)
+                      dc (- nc pc)]]
+            [pos (case [dr dc]
+                   [2 0] #{\W}
+                   [1 1] #{\W \S}
+                   [0 2] #{\S}
+                   [-1 1] #{\S \E}
+                   [-2 0] #{\E}
+                   [-1 -1] #{\E \N}
+                   [0 -2] #{\N}
+                   [1 -1] #{\N \W})]))))
+
+(def boxc {\L \╚ \J \╝ \- \═ \| \║ \7 \╗ \F \╔ \S \S})
+(def norc
+  {#{\N} \↑
+   #{\E} \→
+   #{\W} \←
+   #{\S} \↓
+   #{\N \W} \↖
+   #{\N \E} \↗
+   #{\S \W} \↙
+   #{\S \E} \↘})
+
+(defn vis [path]
+  (let [pretty (into {} (map #(update % 1 boxc) path))
+        max-i (reduce max (map first (keys pretty)))
+        max-j (reduce max (map second (keys pretty)))]
+    (doseq [i (range (inc max-i))]
+      (doseq [j (range (inc max-j))]
+        (print (get pretty [i j] "+")))
+      (print "\n"))))
+
+(defn vis-norm [path]
+  (let [max-i (reduce max (map first (map first path)))
+        max-j (reduce max (map second (map first path)))
+        norms (normals path)]
+    (doseq [i (range (inc max-i))]
+      (doseq [j (range (inc max-j))]
+        (if-let [norm (norms [i j])]
+          (print (norc norm))
+          (print " ")))
+      (print "\n"))))
+
+(defn raytrace-area [path]
+  (let [max-i (reduce max (map first (map first path)))
+        max-j (reduce max (map second (map first path)))
+        norms (normals path)]
+    (reduce +
+     (for [i (range max-i)]
+       (loop [norm-count 0
+              area 0
+              j 0]
+         (if (= j (inc max-j))
+           area
+           (let [norm (norms [i j])]
+             (case norm
+               #{\N \E} (recur (dec norm-count) area (inc j))
+               #{\S \E} (recur (dec norm-count) area (inc j))
+               #{\N \W} (recur (inc norm-count) area (inc j))
+               #{\S \W} (recur (inc norm-count) area (inc j))
+               #{\E} (recur (- norm-count 2) area (inc j))
+               #{\W} (recur (+ norm-count 2) area (inc j))
+               #{\S} (recur norm-count area (inc j))
+               #{\N} (recur norm-count area (inc j))
+               nil (if (zero? norm-count)
+                     (recur norm-count area (inc j))
+                     (recur norm-count (inc area) (inc j)))))))))))
+
+(defn part-two [input]
+  (->> (parse-pipes input)
+       trace-path
+       raytrace-area))
